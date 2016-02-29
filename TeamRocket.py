a81e4ea156e33a70100b6a24d6d2c5e265316cc6
Hantsaniala Eléo <hantsaniala@gmail.com>
2016-02-29 16:37:31 +0300

    Creating the bootstrap of the app

---
+++ TeamRocket.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys, os
+from gui.gui import TR_Gui
+
+def TeamRocket(self):
+    TR_Gui.mainForm(self)
+
+if __name__ == "__main__":
+    TeamRocket(sys.argv)
